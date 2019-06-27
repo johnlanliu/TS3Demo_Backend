1f78b346b903784a79e578cecb614c2a5a6ac250
Jason Lai <jasonlaiusa@yahoo.com>
2019-06-27 16:01:06 -0700

    Added PaymentDetailDto

---
+++ src/main/java/com/anytrek/ts3/dto/PaymentDetailDto.java
@@ -0,0 +1,13 @@
+package com.anytrek.ts3.dto;
+
+import javax.persistence.Column;
+
+import com.anytrek.ts3.model.Payment;
+import com.fasterxml.jackson.annotation.JsonView;
+
+public class PaymentDetailDto extends Payment {
+
+	private static final long serialVersionUID = 85667314860610657L;
+	
+	
+}
