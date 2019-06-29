4af3cfa7a647ae4614c00ece696422f013acac20
Sean Lam <seanlam@berkeley.edu>
2019-06-28 17:11:36 -0700

    added relevant fields

--- src/main/java/com/anytrek/ts3/model/Payment.java
+++ src/main/java/com/anytrek/ts3/model/Payment.java
@@ -30,7 +30,31 @@ public class Payment implements java.io.Serializable {
 	@JsonView(View.Summary.class)
 	@Column(name = "amount", nullable = true, length = 19)
 	private Float amount;
-
+	
+	@JsonView(View.Summary.class)
+	@Column(name = "invoice_no", nullable = true, length = 8)
+	private Integer invoiceNo;
+	
+	@JsonView(View.Summary.class)
+	@Column(name = "customer", nullable = true, length = 100)
+	private String customer;
+	
+	@JsonView(View.Summary.class)
+	@Column(name = "invoice_date", nullable = true, length = 19)
+	private String invoiceDate;
+	
+	@JsonView(View.Summary.class)
+	@Column(name = "due_date", nullable = true, length = 19)
+	private String dueDate;
+	
+	@JsonView(View.Summary.class)
+	@Column(name = "status", nullable = true, length = 7)
+	private String status;
+	
+	@JsonView(View.Summary.class)
+	@Column(name = "sales", nullable = true, length = 100)
+	private String sales;
+	
 	public Integer getPaymentId() {
 		return paymentId;
 	}
@@ -45,6 +69,54 @@ public class Payment implements java.io.Serializable {
 
 	public void setAmount(Float amount) {
 		this.amount = amount;
+	}
+
+	public Integer getInvoiceNo() {
+		return invoiceNo;
+	}
+
+	public void setInvoiceNo(Integer invoiceNo) {
+		this.invoiceNo = invoiceNo;
+	}
+
+	public String getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+
+	public String getInvoiceDate() {
+		return invoiceDate;
+	}
+
+	public void setInvoiceDate(String invoiceDate) {
+		this.invoiceDate = invoiceDate;
+	}
+
+	public String getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getSales() {
+		return sales;
+	}
+
+	public void setSales(String sales) {
+		this.sales = sales;
 	}
 	
 	
